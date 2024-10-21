3bf21fede08a9194c21bd70cdca1d1cb1873a66b
Muhammad Hamza <m.hamza7265@gmail.com>
2024-10-21 15:58:08 +0500

    adding product error corrected

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -45,7 +45,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
         $this->validate($request,[
             'title'=>'string|required',
             'summary'=>'string|required',
